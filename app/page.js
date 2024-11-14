f1ad75b5740fde8f876b5125c94159bc40de7664
Aamir Shahzadah <aamirshahzadah@gmail.com>
2024-11-14 22:11:32 +0500

    Drizzle and Neon

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center">
+    <h1 className="text-center m-20 text-lg text-black">Programming-Communities</h1>
+  </div>
+  
+  );
+}
